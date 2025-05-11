5ad7a07f052ca48758ac3a5890500918b387af5b
winbigbonus <winbigbonus@outlook.co.th>
2025-05-11 11:00:33 +0700

    commit

--- run.ps1
+++ run.ps1
@@ -1,5 +1,8 @@
-﻿cd "C:\Users\Admin\Documents\GitHub\discord-bot-hub"
+﻿# Activate the virtual environment
+C:\Users\Admin\Documents\GitHub\discord-bot-hub\.venv\Scripts\Activate.ps1
 
-"C:\Users\Admin\Documents\GitHub\discord-bot-hub\.venv\Scripts\Activate.ps1"
+# Install the required dependencies
+C:\Users\Admin\Documents\GitHub\discord-bot-hub\.venv\Scripts\python.exe -m pip install -r requirements.txt
 
-& "C:\Users\Admin\Documents\GitHub\discord-bot-hub\.venv\Scripts\python.exe" "C:\Users\Admin\Documents\GitHub\discord-bot-hub\main.py" 
+# Run the main Python script
+& C:\Users\Admin\Documents\GitHub\discord-bot-hub\.venv\Scripts\python.exe C:\Users\Admin\Documents\GitHub\discord-bot-hub\main.py
